010b15ce464d56d1a13242d2448085f2cb60963d
Daniel Cortes <218393764+dani96-pixel@users.noreply.github.com>
2026-07-27 20:16:05 -0500

    Solved the mario-less exercise

--- cs50/week1-c/mario-less/mario.c
+++ cs50/week1-c/mario-less/mario.c
@@ -7,8 +7,27 @@ int main(void)
     int n;
     do
     {
-        n = get_int("Pyramid height: ");
-    } while (n < 1);
+        n = get_int("Height: ");
+    } while (n < 1 || n > 8);
 
     // Print the pyramid
+
+    // For loop to iterate rows and print the jumpspace at the end
+    for (int i = 0; i < n; i++)
+    {
+        // For loop to print the spaces for the row
+        for (int j = 0; j < n - (i + 1); j++)
+        {
+            printf(" ");
+        }
+
+        // For loop to print the hashes for the row
+        for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+
+        // Jumpspace
+        printf("\n");
+    }
 }
