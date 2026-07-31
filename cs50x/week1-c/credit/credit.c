0f8f76e069d17bcee0e273af185da913424fb502
Daniel Cortes <218393764+dani96-pixel@users.noreply.github.com>
2026-07-31 09:47:25 -0500

    Started Solution of credit.c from Problem set 1 of cs50x

---
+++ cs50x/week1-c/credit/credit.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	// Define the result var
+	string result;
+
+	// Ask for a long
+	long long user_card = get_long_long("Number: ");
+
+	// Verify a long
+	// Do the checksum
+	// Do the card company
+	// Get the result
+	// Print result of the long
+	printf("%lli\n", user_card);
+}
