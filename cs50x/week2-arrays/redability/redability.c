eb716a1a0c9b08faac1ee028e62448367582ce11
Daniel Cortes <218393764+dani96-pixel@users.noreply.github.com>
2026-08-08 08:46:20 -0500

    checkpoint: Start proposing sol for redability.c

---
+++ cs50x/week2-arrays/redability/redability.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
+// In a file called readability.c in a folder called readability,
+// you’ll implement a program that calculates the approximate
+// grade level needed to comprehend some text. Your program
+// should print as output “Grade X” where “X” is the grade level
+// computed, rounded to the nearest integer. If the grade level
+// is 16 or higher (equivalent to or greater than a senior
+// undergraduate reading level), your program should output
+// “Grade 16+” instead of giving the exact index number. If the
+// grade level is less than 1, your program should output
+// “Before Grade 1”.
+
+int main(void)
+{
+    // Ask for a text
+    // Call o Create a function to Get the total of words in the text
+    // Call o create a function to get L
+    //{L -> avg number of letters per 100 words}
+    // Call a function to get s
+    //{s }
+}
