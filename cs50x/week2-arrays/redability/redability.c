19bc3bb16ca861c90961c328a1fb6f69fd53dfbb
Daniel Cortes <218393764+dani96-pixel@users.noreply.github.com>
2026-08-09 07:31:39 -0500

    wip: initial attempt for scrabble exercise

--- cs50x/week2-arrays/redability/redability.c
+++ cs50x/week2-arrays/redability/redability.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 
 // In a file called readability.c in a folder called readability,
 // you’ll implement a program that calculates the approximate
@@ -14,12 +15,91 @@
 // grade level is less than 1, your program should output
 // “Before Grade 1”.
 
+// Promises
+int count_words(string s);
+double compute_letters(string s);
+double compute_sentences(string s);
+double compute_coleman_liau_index(double L, double S);
+
+// Main Logic
 int main(void)
 {
-    // Ask for a text
-    // Call o Create a function to Get the total of words in the text
-    // Call o create a function to get L
-    //{L -> avg number of letters per 100 words}
-    // Call a function to get s
-    //{s }
+    string text = get_string("Text: ");
+
+    double letters = compute_letters(text);
+    double sentences = compute_sentences(text);
+    int grade = compute_coleman_liau_index(letters, sentences);
+    if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
+    return 0;
+}
+
+// Custom Functions
+int count_words(string s)
+{
+    int i = 0;
+    int words = 1;
+    while (s[i] != '\0')
+    {
+        if (s[i] == ' ')
+        {
+            words += 1;
+        }
+        i += 1;
+    }
+    return words;
+}
+
+double compute_letters(string s)
+{
+    int i = 0;
+    int number_of_letters = 0;
+    while (s[i] != '\0')
+    {
+        s[i] = tolower(s[i]);
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            number_of_letters += 1;
+        }
+        i += 1;
+    }
+
+    int number_of_words = count_words(s);
+    double letters = (float)number_of_letters / (float)number_of_words * 100;
+    return letters;
+}
+
+double compute_sentences(string s)
+{
+    int i = 0;
+    int number_of_sentences = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] == '.' || s[i] == '!' || s[i] == '?')
+        {
+            number_of_sentences += 1;
+        }
+        i += 1;
+    }
+
+    int number_of_words = count_words(s);
+    double sentences = (float)number_of_sentences / (float)number_of_words * 100;
+    return sentences;
+}
+
+double compute_coleman_liau_index(double L, double S)
+{
+    double index = 0.0588 * L - 0.296 * S - 15.8;
+    index = round(index);
+    return index;
 }
